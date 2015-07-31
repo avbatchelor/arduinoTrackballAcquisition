c8b81a43a3393a63f06391a58bc89a05cedbe5ae
avbatchelor <abatchelor@fas.harvard.edu>
2015-07-30 20:04:32 -0400

    proper online displacement plotting

--- acquireTrackballDataOnline.m
+++ acquireTrackballDataOnline.m
@@ -17,6 +17,8 @@ inChannelsUsed = 0:1;
 % daqreset;
 devID = 'Dev1';
 sampRate = 40E3;
+accumData = [];
+accumTime = [];
 
 %% Configure input session
 sIn = daq.createSession('ni');
@@ -33,34 +35,33 @@ lh = sIn.addlistener('DataAvailable', @plotData);
 
     function plotData(~,event) 
         settings = ballSettings; 
-        [vel(:,1),disp(:,1)] = getVel(event.Data(:,1),settings.xMinVal,settings.xMaxVal,settings,sampRate);
-        [vel(:,2),disp(:,2)] = getVel(event.Data(:,2),settings.yMinVal,settings.yMaxVal,settings,sampRate);
+        accumData = [accumData;event.Data];
+        accumTime = [accumTime;event.TimeStamps];
+        
+        [vel(:,1),disp(:,1)] = getVel(accumData(:,1),settings.xMinVal,settings.xMaxVal,settings,sampRate);
+        [vel(:,2),disp(:,2)] = getVel(accumData(:,2),settings.yMinVal,settings.yMaxVal,settings,sampRate);
         figure(1)
 %        plot(event.TimeStamps, cumsum((event.Data-2048).*mmConv./sensorRes))
         subplot(4,1,1)
-        plot(event.TimeStamps, vel(:,1))
-%         plot(event.TimeStamps, event.Data(:,1))
-        hold on 
+        plot(accumTime, vel(:,1))
+%         plot(accumTime, event.Data(:,1))
         title('Vx')
         subplot(4,1,2)
-        plot(event.TimeStamps, vel(:,2))
-%         plot(event.TimeStamps, event.Data(:,2))
-        hold on 
+        plot(accumTime, vel(:,2))
+%         plot(accumTime, event.Data(:,2))
         title('Vy')
         subplot(4,1,3)
-        plot(event.TimeStamps, disp(:,1))
-        hold on 
+        plot(accumTime, disp(:,1))
         title('X Disp')
         subplot(4,1,4)
-        plot(event.TimeStamps, disp(:,2))
-        hold on 
+        plot(accumTime, disp(:,2))
         title('Y Disp')
         
     end
 
 
 %% Run trials
-sIn.NotifyWhenDataAvailableExceeds = 0.1*sIn.Rate;
+sIn.NotifyWhenDataAvailableExceeds = 1*sIn.Rate;
 %sIn.IsNotifyWhenDataAvailableExceedsAuto
 
 sIn.startBackground;
